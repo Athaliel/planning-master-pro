b948503467583faf095b88390dda6b62d7c88efd
Athaliel <duval.alix@gmail.com>
2025-07-10 15:42:04 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"; // <-- important
+import { getFirestore } from "firebase/firestore"; // 🔄 Firestore au lieu de Realtime
 
 const firebaseConfig = {
   apiKey: "AIzaSyBg4TA9l8AbJU2e_Gn7Qi_Cd1ugjlfy2SM",
   authDomain: "planning-master-pro.firebaseapp.com",
   projectId: "planning-master-pro",
-  storageBucket: "planning-master-pro.firebasestorage.app",
+  storageBucket: "planning-master-pro.appspot.com",
   messagingSenderId: "676881441699",
-  appId: "1:676881441699:web:5e955d668e6fa8d1f77016",
-  databaseURL: "https://planning-master-pro-default-rtdb.europe-west1.firebasedatabase.app" // <-- ajoute bien cette ligne
+  appId: "1:676881441699:web:5e955d668e6fa8d1f77016"
+  // ❌ Pas de databaseURL ici pour Firestore
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getFirestore(app);
 
-export { database };
+export { db };
